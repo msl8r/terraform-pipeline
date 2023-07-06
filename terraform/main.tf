6492df07f425c21970499ebf518f46b12ff73b64
matt <matt@slater.in>
2023-07-06 21:30:52 +0100

    Change resources rg tf name

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "resources" {
 resource "azurerm_key_vault" "this" {
   location                  = "uksouth"
   name                      = "infra-secrets"
-  resource_group_name       = azurerm_resource_group.this.name
+  resource_group_name       = azurerm_resource_group.networking.name
   sku_name                  = "standard"
   tenant_id                 = "02a438dc-5f2b-4617-a0cc-c7b4e199dcaa"
   enable_rbac_authorization = true
