d9dc779ef87a0f4fcbb0e9564efc35e65b85c056
matt <matt@slater.in>
2023-07-06 21:47:50 +0100

    try using vars

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,12 +8,12 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "terraform-state"
-    storage_account_name = var.storage_account_name
+#    storage_account_name = var.storage_account_name
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    subscription_id      = var.subscription_id
-    tenant_id            = var.tenant_id
-    use_oidc             = true
+#    subscription_id      = var.subscription_id
+#    tenant_id            = var.tenant_id
+#    use_oidc             = true
   }
 }
 
